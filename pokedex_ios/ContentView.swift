764207d61df5183699f02500036b9193819d44de
Carboncito <carboncito760@gmail.com>
2023-07-04 22:30:21 -0600

    init proyect

---
+++ pokedex_ios/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  pokedex_ios
+//
+//  Created by Jonathan Hernández on 04/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pokedex_ios/pokedex_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedex_iosApp.swift
+//  pokedex_ios
+//
+//  Created by Jonathan Hernández on 04/07/23.
+//
+
+import SwiftUI
+
+@main
+struct pokedex_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
